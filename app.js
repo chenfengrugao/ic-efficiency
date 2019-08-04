a5a70f60a579275da4eb1b8f5c6ca7ff8cf1f599
chenfengrugao <chenfengrugao@yahoo.com>
2019-08-04 19:04:53 +0800

    update 2019-8-4

--- app.js
+++ app.js
@@ -1,103 +1,30 @@
 
 var express = require('express');
-var fs = require("fs");
-var url = require("url");
-//var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-//create express obj
 var app = express();
-app.use('/public', express.static('public')); //static router
-app.set('views', 'views');
+var router = express.Router();
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-//app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended: false}));
 
-//load json files
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+var indexRouter = require('./routes/index');
+var loginRouter = require('./routes/login');
+var mainRouter = require('./routes/main');
+var newIssueRouter = require('./routes/newIssue');
+app.use('/', indexRouter);
+app.use('/login', loginRouter);
+app.use('/main', mainRouter);
+app.use('/newIssue', newIssueRouter);
 
-app.get('/', function(req, res){
-    res.render('index', {"ver": "0.1.0"});
-});
-
-app.post('/login', function(req, res){
-    var username = req.body.username;
-    var passwd = req.body.passwd;
-    //console.log("u:" + username + "p:" + passwd);
-    if(username == "chenf" && passwd == "123456"){
-	res.send("登录成功");
-    }else{
-	res.send("用户名或密码错误");
-    }
-});
-
-app.get('/add', function(req, res){
-    res.render('add', {});
-});
-
-
-app.get('/add_handle', function(req, res){
-    var params = url.parse(req.url, true).query;
-    params.datetime = new Date().toLocaleString();
-    global.word.data.push(params);
-    res.send("添加成功");
-});
-
-
-app.get('/modify', function(req, res){
-    var params = url.parse(req.url, true).query;
-    var word = params.w
-    var phonetic = "";
-    var explanation = "";
-    for(var i=0; i<global.word.data.length; i++){
-	if(global.word.data[i].name == params.w){
-	    //console.log(global.word.data[i]);
-	    phonetic = global.word.data[i].phonetic;
-	    explanation = global.word.data[i].explanation;
-	    sentence = global.word.data[i].sentence;
-	    break;
-	}
-    }
-    res.render('modify', {"name": word, "phonetic": phonetic, "explanation": explanation, "sentence": sentence});
-});
-
-app.get('/modify_handle', function(req, res){
-    var params = url.parse(req.url, true).query;
-    var word = params.name;
-    var phonetic = params.phonetic;
-    var explanation = params.explanation;
-    var sentence = params.sentence;
-    for(var i=0; i<global.word.data.length; i++){
-	if(global.word.data[i].name == word){
-	    global.word.data[i].phonetic = phonetic;
-	    global.word.data[i].explanation = explanation;
-	    global.word.data[i].sentence = sentence;
-	    break;
-	}
-    }
-    
-    res.send("修改成功");
-});
-
-
-app.get('/delete', function(req, res){
-    var params = url.parse(req.url, true).query;
-    var word = params.w
-    for(var i=0; i<global.word.data.length; i++){
-	if(global.word.data[i].name == word){
-	    global.word.data.splice(i, 1);
-	    break;
-	}
-    }
-    res.send("删除成功");
-});
-
-
-app.get('/save_handle', function(req, res){
-    var s = JSON.stringify(word);
-    fs.writeFileSync('word.json', s);
-    res.send("success");
-});
 
 var httpServer = app.listen(3001, function(){
     console.log("server running at http://127.0.0.1:3001");
